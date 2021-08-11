cf12fd4e0dd6e56664ef81a804b2d3cb3fe5a545
Pranav Gade <pranavgade20@gmail.com>
2021-08-11 16:27:28 +0530

    added script
    and deleted duplicate file

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -o errexit
+
+convert $(find | grep img/Screenshot | sort -n | paste -sd\ ) slides-debug.pdf
+
+mkdir -p cropped/img
+for img in img/*.png; do
+	convert -compress jpeg -quality 70 -crop 1595x901+880+365 $img "cropped/$img.jpg"
+done
+
+convert $(find | grep cropped/img/Screenshot | sort -n | paste -sd\ ) slides.pdf
+
+rm -r cropped
